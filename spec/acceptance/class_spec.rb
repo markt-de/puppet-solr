a0655e36ef86c0475fdb3780978ce939e443df8c
Frank Wall <fw@moov.de>
2022-08-15 23:38:12 +0200

    handle more acceptance test failures

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -3,6 +3,17 @@ require 'spec_helper_acceptance'
 describe 'solr class' do
   before(:all) do
     apply_manifest(%(
+      # Tests will fail if `ss` is not installed.
+      if ($facts['os']['family'] == 'RedHat') and (versioncmp($facts['os']['release']['major'], '8') >= 0) {
+        ensure_packages('iproute')
+      }
+
+      if ($facts['os']['family'] == 'Debian') {
+        $target_path = '/usr/lib/jvm'
+      } else {
+        $target_path = '/usr/java'
+      }
+
       java::adopt { 'jdk11':
         ensure        => 'present',
         java          => 'jdk',
@@ -11,7 +22,7 @@ describe 'solr class' do
       }
       -> file { '/usr/bin/java':
         ensure => link,
-        target => '/usr/java/jdk-11.0.6+10/bin/java',
+        target => "${target_path}/jdk-11.0.6+10/bin/java",
       }
     ), catch_failures: true)
   end
